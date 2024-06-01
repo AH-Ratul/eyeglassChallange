7c93c11106bdaafe9307531bbd094421dbc53efa
AH-Ratul <ahratul21@gmail.com>
2024-06-02 01:46:24 +0600

    section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Header from '../components/Header/Header';
-import HeaderSm from '../components/Header/Header_sm';
+import React from "react";
+import Header from "../components/Header/Header";
+import HeaderSm from "../components/Header/Header_sm";
+import Hero from "../components/Hero/Hero";
 
 const Main = () => {
-    return (
-        <div className=' relative'>
-            {/* Header section */}
-            <section>
-                <Header />
-                <HeaderSm />
-            </section>
+  return (
+    <div className="min-h-[100dvh] relative">
+      {/* Header section */}
+      <section>
+        <Header />
+        <HeaderSm />
+      </section>
 
-            {/* Body Section */}
-            <section>
-                
-            </section>
-        </div>
-    );
+      {/* Body Section */}
+      <section>
+        <Hero />
+      </section>
+    </div>
+  );
 };
 
 export default Main;
