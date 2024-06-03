fe4d4a4ddeb5917a92ae05e1c019e3e5e132192d
AH-Ratul <ahratul21@gmail.com>
2024-06-04 01:49:16 +0600

    product data file added

---
+++ public/productData.js
@@ -0,0 +1,53 @@
+import img1 from "../src/assets/Image (8).png";
+import img2 from "../src/assets/Image (7).png";
+import img3 from "../src/assets/Image (10).png";
+import img4 from "../src/assets/Image (9).png";
+
+export const products = [
+  {
+    id: 1,
+    productName: "Bravo Sunglasses",
+    price: 150,
+    afterSalePrice: 100,
+    productSale: 10,
+    image: img1,
+  },
+  {
+    id: 2,
+    productName: "Vincent Chase",
+    price: 150,
+    afterSalePrice: 120,
+    productSale: 10,
+    image: img2,
+  },
+  {
+    id: 3,
+    productName: "John Jacobs",
+    price: 100,
+    productSale: 0,
+    image: img2,
+  },
+  {
+    id: 4,
+    productName: "Tommy Hilfiger",
+    price: 150,
+    afterSalePrice: 130,
+    productSale: 10,
+    image: img3,
+  },
+  {
+    id: 5,
+    productName: "Ray Ban",
+    price: 100,
+    afterSalePrice: 0,
+    image: img4,
+  },
+  {
+    id: 6,
+    productName: "Occhiali",
+    price: 150,
+    afterSalePrice: 110,
+    productSale: 10,
+    image: img1,
+  },
+];
