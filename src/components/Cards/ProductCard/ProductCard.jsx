b2268f727b5f3ebfe7b559435e60d6822337e37f
AH-Ratul <ahratul21@gmail.com>
2024-06-19 23:29:55 +0600

    slider updates

--- src/components/Cards/ProductCard/ProductCard.jsx
+++ src/components/Cards/ProductCard/ProductCard.jsx
@@ -4,60 +4,46 @@ import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/autoplay";
 
+import "../../Hero/module.css";
 
 const ProductCard = ({ item }) => {
   const { price, productName, image, afterSalePrice, productSale } = item;
   const { love } = allIcons;
 
   return (
-    <main className="mt-9 px-2 text-textH1">
+    <main className="px-2 text-textH1">
       <section className="px-8">
-        <Swiper
-        direction="horizontal"
-        slidesPerView={1}
-        spaceBetween={30}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-        modules={[Pagination, Autoplay]}
-        className="h-full w-full"
-        >
-          <SwiperSlide>
-            <div className="relative bg-gr3 h-[350px] flex  justify-center items-center w-80">
-              <img src={image} alt="image" className="w-[280px]" />
-              <div className="flex justify-between items-center  absolute top-4 w-full px-5">
+        <div className="w-[320px] overflow-hidden">
+          {/* horizontal slider */}
+          <Swiper
+            direction={"horizontal"}
+            slidesPerView={1}
+            spaceBetween={30}
+            pagination={{ clickable: true }}
+            autoplay={false}
+            modules={[Pagination, Autoplay]}
+            className="h-full w-full"
+          >
+            <SwiperSlide className="relative bg-gr3 h-[350px] flex  justify-center items-center w-80">
+              <div className="flex justify-between items-center  absolute top-0 w-full px-5">
                 {productSale > 0 ? (
-                  <div className="text-base font-semibold bg-crm1 h-12 w-12 flex justify-center items-center rounded-full">
+                  <div className="absolute top-[30px] text-base font-semibold bg-crm1 h-12 w-12 flex justify-center items-center rounded-full">
                     {productSale}%
                   </div>
                 ) : (
                   ""
                 )}
-                <div className="border border-gr2 h-12 w-12 flex justify-center items-center text-2xl rounded-full">
+                <div className="absolute top-[30px] right-[30px] border border-gr2 h-12 w-12 flex justify-center items-center text-2xl rounded-full">
                   {love}
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative bg-gr3 h-[350px] flex  justify-center items-center w-80">
-              <img src={image} alt="image" className="w-[280px]" />
-              <div className="flex justify-between items-center  absolute top-4 w-full px-5">
-                {productSale > 0 ? (
-                  <div className="text-base font-semibold bg-crm1 h-12 w-12 flex justify-center items-center rounded-full">
-                    {productSale}%
-                  </div>
-                ) : (
-                  ""
-                )}
-                <div className="border border-gr2 h-12 w-12 flex justify-center items-center text-2xl rounded-full">
-                  {love}
-                </div>
+              <div className="w-[280px] absolute mx-auto">
+                <img src={image} alt="image" />
               </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+            </SwiperSlide>
+          </Swiper>
+        </div>
 
         {/* details */}
         <div className="my-5">
