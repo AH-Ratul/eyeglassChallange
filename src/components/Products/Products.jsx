d1df4caecfd074945b0732f68d341cab67d53cc9
AH-Ratul <ahratul21@gmail.com>
2024-06-19 23:32:09 +0600

    changes for large screen

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,15 +7,15 @@ const Products = () => {
   //
   return (
     <main className="absolute top-[2600px] sm:top-[2900px] lg:top-[2650px] ">
-      <section className="flex flex-col justify-center items-center lg:flex-row text-textH1 px-4">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-2xl mb-6">Our Products</h1>
-          <p className="text-center">
+      <section className="flex flex-col  md:justify-between items-center md:flex-row text-textH1 px-4">
+        <div className="flex flex-col  items-center md:items-start pt-0 md:pl-[100px]">
+          <h1 className="font-bold text-2xl mb-6 ">Our Products</h1>
+          <p className="text-center md:text-start md:text-sm md:w-[410px]">
             Experience crystal clear vision and elevated style with our premium
             collection of eyeglasses.
           </p>
         </div>
-        <div className="mt-8 flex items-center font-semibold">
+        <div className="mt-8 flex items-center font-semibold md:pr-[100px]">
           <div>Latest</div>
           <div className="mr-7 ml-7 border border-gr2 rounded px-4 py-1">
             Special
@@ -23,7 +23,7 @@ const Products = () => {
           <div>Best Sell</div>
         </div>
       </section>
-      <section>
+      <section className="md:flex md:flex-wrap md:justify-center md:items-center mt-9">
         {data.map((item) => (
           <ProductCard key={item.id} item={item}></ProductCard>
         ))}
